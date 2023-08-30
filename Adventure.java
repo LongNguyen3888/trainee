e100e8a1ddc636dadb1f21049613ad61876cb823
LongNguyen3888 <131256471+LongNguyen3888@users.noreply.github.com>
2023-08-30 08:58:47 +0700

    Update Adventure.java

--- Adventure.java
+++ Adventure.java
@@ -1,3 +1,17 @@
+/*Đoàn thám hiểm Nam cực xuất phát từ Trạm nghiên cứu đặt ở điểm có toạ độ (0, 0). Trục OX chạy từ tây sang đông, trục OY chạy từ nam lên bắc. 
+Ban đầu Đoàn đi theo một trong số 4 hướng Đông (E), Tây (W), Nam (S) hoặc Bắc (N). Sau khi đi một quảng đường là một đơn vị độ dài Đoàn dừng lại 
+thực hiện các đo đạc khảo sát cần thiết và tuỳ theo tình hình thực tế, trưởng đoàn sẽ quyết định đi tiếp theo hướng nào. Để đánh dấu các điểm trên 
+bản đồ khai thác và dễ dàng xác định đường trở về lịch trình di chuyển trong nhật ký được ghi dưới dạng xâu các ký tự trong tập {E, W, S, N}. 
+Tuy vậy thời tiết ở địa cực nổi tiếng là đỏng đảnh. Bão tuyết có thể nổi lên bất kỳ lúc nào kèm theo các cơn bão từ làm rối loạn la bàn. 
+Khi đó đường đi sẽ được ghi nhận bằng các ký tự L - Rẽ trái, R - Rẽ phải, B – Đi ngược lại hướng vừa đi hoặc C - Tiếp tục đi thẳng theo hướng cũ. 
+Mỗi khi bão từ chấm dứt, la bàn hoạt động bình thường trở lại lịch trình di chuyển lại được ghi nhận bằng các ký tự E, W, S, N. 
+Ở thời điểm ban đầu của cuộc thám hiểm thời tiết tốt, không có bão từ. Trong suốt cuộc hành trình có thể không có trận bão từ nào hoặc bão từ 
+có thể xẩy ra nhiều lần. Ví dụ, lịch trình ở hình bên có thể được ghi nhận bằng xâu WRECSSCRWWCRN.
+Yêu cầu: Cho một lịch trình di chuyển xác định bởi xâu Z độ dài không quá 200 gồm các ký tự trong tập {E, W, S, N, L ,R, B, C}, bắt đầu 
+bằng một trong số 4 ký tự đầu tiên. Hãy tính khoảng cách theo đường chim bay từ điểm dừng cuối của đoàn thám hiểm tớỉ Trạm nghiên cứu mà Đoàn xuất phát. 
+Dữ liệu: Vào từ file văn bản EXPLORE.INP gồm một dòng chứa xâu Z xác định một lịch trình di chuyển của đoàn thám hiểm.
+Kết quả: Đưa ra file văn bản EXPLORE.OUT một số thực có 3 chữ số sau dấu chấm thập phân, đó là khoảng cách tìm được với dữ liệu vào đã cho. */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
